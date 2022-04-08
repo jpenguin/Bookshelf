b4bba28767f473aa5d5250a512b92b05f5ebd779
jpenguin <jpenguin@fastmail.com>
2022-04-08 12:19:40 -0700

    Rest to local files

--- Book.h
+++ Book.h
@@ -1,4 +1,4 @@
-// Copyright 2022 Joshua M Dye. Licensed under the Educational
+//Copyright 2022 Joshua M Dye Licensed under the Educational
 //Community License, Version 2.0 (the "License"); you may not use this file
 //except in compliance with the License. You may obtain a copy of the License at
 //
@@ -24,7 +24,4 @@ struct Book {
   string author;
   string subject;
 };
-
-
-
 #endif //BOOKSHELF__BOOK_H_
