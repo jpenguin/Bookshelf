b764c03726e29ad14df8221bbba26c1e07e300ed
jpenguin <jpenguin@fastmail.com>
2022-04-08 11:14:42 -0700

    Compiles, need to add comments, possibly move code around

--- Shelf.h
+++ Shelf.h
@@ -1,6 +1,14 @@
+// Copyright 2022 Joshua M Dye. Licensed under the Educational
+//Community License, Version 2.0 (the "License"); you may not use this file
+//except in compliance with the License. You may obtain a copy of the License at
 //
-// Created by jpenguin on 4/7/22.
+//http://opensource.org/licenses/ECL-2.0
 //
+// Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+//WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+//License for the specific language governing permissions and limitations under
+//the License.
 
 #ifndef BOOKSHELF__SHELF_H_
 #define BOOKSHELF__SHELF_H_
--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
-//
-// Created by jpenguin on 4/6/22.
-//
+/*******************************************************************************
+ * program name: cis65 programming assignment 4
+ * created date: Apr 06 2022
+ * created by: josh m dye
+ * purpose: Write a program that maintains a list of books for a library.
+ * The program must accept commands to make insertions and deletions in this
+ * list, while simultaneously maintaining the list in alphabetical order for
+ * three string type keys: Subject, Author, and Title.
+ * copyright 2022 Josh M Dye licensed under the educational community license,
+ * version 2.0 (the "license"); you may not use this file except in compliance
+ * with the license.you may obtain a copy of the license at
+ * http://www.osedu.org/licenses/ecl-2.0 unless required by applicable law or
+ * agreed to in writing, software distributed under the license is distributed
+ * on an "as is" basis, without warranties or conditions of any kind, either
+ * express or implied. see the license for the specific language governing
+ * permissions and limitations under the license.
+ *******************************************************************************/
+
 #include <iostream>
 #include "Shelf.h"
 
